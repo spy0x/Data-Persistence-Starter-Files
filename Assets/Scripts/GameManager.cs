b203e5d6b6d9f7ae4f7c383e1a00ee154b0db829
Francisco V. Diaz <fevarga1@uc.cl>
2021-11-22 12:19:02 -0300

    created GameManager Class . Created StartPlayScene method.,

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] TMP_InputField inputField;
+    private static string playerName;
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+    public static string PlayerName { get { return playerName; } }
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void StartPlayScene()
+    {
+        playerName = inputField.text;
+        Debug.Log($"El nombre del player es: {playerName}");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
